4c6fcc50287eba628ccec12a4421da4369398152
songkeys <i@song.work>
2025-03-24 22:16:08 +0800

    Add PostgreSQL support and update .gitignore

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,8 +1,13 @@
 import { betterAuth } from "better-auth";
 import { anonymous } from "better-auth/plugins/anonymous";
 import { passkey } from "better-auth/plugins/passkey";
+// @ts-expect-error no types
+import { Pool } from "pg";
 
 export const auth = betterAuth({
+	database: new Pool({
+		connectionString: process.env.DATABASE_URL,
+	}),
 	plugins: [
 		anonymous(),
 		// https://www.better-auth.com/docs/plugins/passkey
